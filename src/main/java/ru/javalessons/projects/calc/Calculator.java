5b8b76a3747eb4f104588fc476654ec65cb869fc
RomaMiddle <batichta@yandex.ru>
2022-02-14 22:38:56 +0400

    change

--- src/main/java/ru/javalessons/projects/calc/Calculator.java
+++ src/main/java/ru/javalessons/projects/calc/Calculator.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 class CalculatorFrame extends JFrame {
     public CalculatorFrame() {
-        setTitle("Калькулятор java-lessons.ru");
+        setTitle("Калькулятор java");
         CalculatorPanel panel = new CalculatorPanel();
         add(panel);
         pack();
