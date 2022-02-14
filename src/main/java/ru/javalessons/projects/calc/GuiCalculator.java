48e50858339b15fb34a6f826776e831f773a9457
RomaMiddle <batichta@yandex.ru>
2022-02-14 22:36:14 +0400

    Initial commit

---
+++ src/main/java/ru/javalessons/projects/calc/GuiCalculator.java
@@ -0,0 +1,16 @@
+package ru.javalessons.projects.calc;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GuiCalculator {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                CalculatorFrame frame = new CalculatorFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
